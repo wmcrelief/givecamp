f88b6fbf96f4e0f3a62bc61023aa2175846f4b40
Topher <topher@derosia.com>
2016-11-04 19:47:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WMCR Child Theme
---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Make Child
+ */
+
+/**
+ * The theme version.
+ */
+define( 'TTFMAKE_CHILD_VERSION', '1.1.0' );
+
+/**
+ * Turn off the parent theme styles.
+ *
+ * If you would like to use this child theme to style Make from scratch, rather
+ * than simply overriding specific style rules, simply remove the '//' from the
+ * 'add_filter' line below. This will tell the theme not to enqueue the parent
+ * stylesheet along with the child one.
+ */
+//add_filter( 'make_enqueue_parent_stylesheet', '__return_false' );
+
+/**
+ * Add your custom theme functions here.
+ */
